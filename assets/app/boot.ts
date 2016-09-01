122fc76cd3e4e8160998abeb8cfe8d11b9294f2c
hvpulok <hvpulok@gmail.com>
2016-08-31 22:08:21 -0600

    Feat: Add router location strategy, HashLocationStartegy

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -3,5 +3,17 @@ import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {MessageService} from "./messages/message.service";
 import {ROUTER_PROVIDERS} from "angular2/router";
+import {provide} from "angular2/src/core/di/provider";
+import {LocationStrategy} from "angular2/src/router/location/location_strategy";
+import {HashLocationStrategy} from "angular2/src/router/location/hash_location_strategy";
 
-bootstrap(AppComponent, [MessageService, ROUTER_PROVIDERS]);
+bootstrap(
+    AppComponent,
+    [
+        MessageService,
+        ROUTER_PROVIDERS,
+        provide(LocationStrategy, {useClass: HashLocationStrategy}),
+
+    ],
+
+);
